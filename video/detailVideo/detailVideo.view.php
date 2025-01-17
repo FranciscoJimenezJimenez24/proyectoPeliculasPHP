f2eeeb09db9078b759ae33063580d111a0529f6a
FranciscoJimenezJimenez24 <franjimenezjimenez24@gmail.com>
2025-01-17 18:03:20 +0100

    icon

--- video/detailVideo/detailVideo.view.php
+++ video/detailVideo/detailVideo.view.php
@@ -16,6 +16,7 @@ if (isset($_SESSION['alert'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../../assets/icoFilm.svg" type="image/x-icon">
     <link rel="stylesheet" href="detailVideo.css">
     <title>Detalle video</title>
 </head>
